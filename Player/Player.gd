f9e7d4017ca2aac355b8e867a5952fffc9aa976b
jsalaunpenquer <56338150+jsalaunpenquer@users.noreply.github.com>
2021-07-31 19:34:42 +0200

    Add enemy and first interact

--- Player/Player.gd
+++ Player/Player.gd
@@ -19,9 +19,11 @@ var roll_vector = Vector2.LEFT
 onready var animationPlayer = $AnimationPlayer 
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox = $HitboxPivot/SwordHitbox
 
 func _ready():
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
 # Main loop (n every single physic step)
 func _physics_process(delta):
@@ -47,6 +49,7 @@ func move_state(delta):
 	#When the player is  moving...
 	if input_vector != Vector2.ZERO:
 		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
